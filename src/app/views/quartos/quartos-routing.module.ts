ad79c86fb62fb0f0bb61ef90df67d0be3d960fb6
Lisi Tomelin <lisa_tomelin@hotmail.com>
2024-06-20 11:12:46 -0300

    add: rotas

--- src/app/views/quartos/quartos-routing.module.ts
+++ src/app/views/quartos/quartos-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { InserirQuartosComponent } from './inserir-quartos/inserir-quartos.component';
 import { ListarQuartosComponent } from './listar-quartos/listar-quartos.component';
+import { EditarQuartosComponent } from './editar-quartos/editar-quartos.component';
+import { ExcluirQuartosComponent } from './excluir-quartos/excluir-quartos.component';
 
 const routes: Routes = [
   {
@@ -16,12 +18,19 @@ const routes: Routes = [
   {
     path: 'inserir',
     component: InserirQuartosComponent,
-   
+  },
+  {
+    path: 'editar/:id',
+    component: EditarQuartosComponent,
+  },
+  {
+    path: 'excluir/id',
+    component: ExcluirQuartosComponent,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class QuartosRoutingModule { }
+export class QuartosRoutingModule {}
